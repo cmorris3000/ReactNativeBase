af01c30bd89a44430804c99e29b68e48d927d940
cmorris3000 <cmorris3000@gmail.com>
2019-10-22 02:03:51 -0300

    Add header in home page

--- src/screems/Home.js
+++ src/screems/Home.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import {Button} from 'native-base';
+import { StyleSheet, Text, View, Platform} from 'react-native';
+import {Container, Button, Header, Left, Body, Right, Title, Subtitle, Icon, Item, Input} from 'native-base';
 
 export default class Home extends React.Component{
+  state={
+    searchText: ""
+  }
+  IconSearch= ()=>{
+    
+  }
   render(){
     return(
-      <View style={styles.container}>
+      
+        <View style={styles.container}>
+          <Header searchBar rounded>
+            <Left>         
+                <Icon style={styles.searchIcon} name='arrow-back' onPress={()=> this.props.switchScreem("Login")}/>  
+            </Left>
+            <Body>
+              <Title>Home</Title>              
+            </Body>
+            <Right>              
+              <Button transparent>
+                <Icon name='heart' />
+              </Button>            
+              <Button transparent>
+                <Icon style={styles.searchIcon}ios='ios-menu' android="md-menu" />
+              </Button>              
+            </Right>
+            
+            
+          </Header>
+          <Item>
+              <Icon ios='ios-search' android="md-search" onPress={this.IconSearch}/>
+              <Input placeholder="Search" value={this.searchText} onChangeText={(search)=>this.setState({searchText: search})}/>
+          </Item>
         <Text>Welcome to Home Page</Text>
         <Button onPress={()=> this.props.switchScreem("Login")}>
           <Text style={styles.buttonText}>Return</Text></Button>
-      </View>
+        </View>
+        
+        
+      
     )
   }
 }
@@ -17,11 +49,14 @@ export default class Home extends React.Component{
 const styles= StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    
+  },  
+  searchIcon:{
+    
+    color: Platform.OS === 'ios' ? 'black' : 'white'
   },
   buttonText:{
     marginLeft: 6,
     marginRight: 6
-  }
+  },
 })
